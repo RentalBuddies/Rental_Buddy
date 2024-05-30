3b8f6e4561b837cca31056c19a11005059e57a07
mYsTeRiOOOOOO <prabhavverma2@gmail.com>
2024-05-30 11:54:47 +0530

    Added

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const methodOverride = require("method-override");
 const path = require("path");
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
 
 
 app.set("view engine","ejs");
@@ -12,6 +14,17 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.set("view engine" ," ejs");
 app.use("/css",express.static("dist"));
+
+main().then((res)=>{
+    console.log("success  connected to DB");
+  })
+  .catch(err => console.log(err));
+  
+  async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/Rental_Buddy');
+  
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  }
 
 
 app.listen("8080",()=>{
